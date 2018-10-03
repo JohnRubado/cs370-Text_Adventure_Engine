8524e331ecf6eac0e6f42e0a6f650ca45a15c394
BuildTools <unconfigured@null.spigotmc.org>
2018-10-02 20:22:49 -0400

    Initial transitions implementation

--- packages/transition/transition.py
+++ packages/transition/transition.py
@@ -1,14 +1,7 @@
-
-
 class transition:
 
-    def __init__(self,name,area,destination,cardinalPosition,possibleActions):
+    def __init__(self,name,area,destination):
         self.name = name;
         self.message ="";
         self.area = area;
         self.destination = destination;
-        self.cardinalPosition = cardinalPosition;
-        if len(possibleActions) >= 1:
-            self.possibleActions = possibleActions;
-        else:
-            self.possibleActions = ["use"];
